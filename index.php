4b732b329ca9e271bdaccf94e332bbd6fe8465a3
FlorianePuy <77892356+FlorianePuy@users.noreply.github.com>
2024-04-16 14:11:01 +0200

    ajout de bootstrap

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="utf-8">
     <title>Titre de la page</title>
+    <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/override-bootstrap.css">
     <title>NutriTrendz</title>
 </head>
-
 <body>
 
+<script type="text/javascript" src="lib/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
