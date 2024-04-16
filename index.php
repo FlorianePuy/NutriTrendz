df563a942f709d456c6258fb379318ed37cc8e5c
FlorianePuy <77892356+FlorianePuy@users.noreply.github.com>
2024-04-16 14:36:47 +0200

    ajout du haeder

--- index.php
+++ index.php
@@ -2,12 +2,32 @@
 <html lang="fr">
 <head>
     <meta charset="utf-8">
-    <title>Titre de la page</title>
     <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/override-bootstrap.css">
     <title>NutriTrendz</title>
 </head>
 <body>
+<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between mb-4 px-2
+border-bottom">
+    <div class="col-md-3 mb-2 mb-md-0">
+        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+            <img class="w-25" src="assets/img/NutriTrendz-logo-removedBG.png" alt="logo du blog">
+        </a>
+    </div>
+
+    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+        <li><a href="#" class="nav-link px-2">Features</a></li>
+        <li><a href="#" class="nav-link px-2">Pricing</a></li>
+        <li><a href="#" class="nav-link px-2">FAQs</a></li>
+        <li><a href="#" class="nav-link px-2">About</a></li>
+    </ul>
+
+    <div class="col-md-3 text-end">
+        <button type="button" class="btn btn-outline-success me-2">Login</button>
+        <button type="button" class="btn btn-success">Sign-up</button>
+    </div>
+</header>
 
 <script type="text/javascript" src="lib/bootstrap/js/bootstrap.min.js"></script>
 </body>
