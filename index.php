9afa407653f7dd89dd08e71ed91d512e4a58533d
FlorianePuy <77892356+FlorianePuy@users.noreply.github.com>
2024-04-16 13:48:16 +0200

    création du projet

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <link rel="stylesheet" href="assets/css/override-bootstrap.css">
+    <title>NutriTrendz</title>
+</head>
+
+<body>
+
+</body>
+</html>
