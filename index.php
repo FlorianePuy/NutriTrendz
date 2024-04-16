b32cdf737b85e9ec4bfda3def3a857db344fe328
FlorianePuy <77892356+FlorianePuy@users.noreply.github.com>
2024-04-16 15:59:59 +0200

    ajout de l'accueil

--- index.php
+++ index.php
@@ -10,25 +10,41 @@
 <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between mb-4 px-2
 border-bottom">
     <div class="col-md-3 mb-2 mb-md-0">
-        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+        <a href="#" class="d-inline-flex link-body-emphasis text-decoration-none">
             <img class="w-25" src="assets/img/NutriTrendz-logo-removedBG.png" alt="logo du blog">
         </a>
     </div>
 
     <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
         <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
-        <li><a href="#" class="nav-link px-2">Features</a></li>
-        <li><a href="#" class="nav-link px-2">Pricing</a></li>
-        <li><a href="#" class="nav-link px-2">FAQs</a></li>
-        <li><a href="#" class="nav-link px-2">About</a></li>
+        <li><a href="#" class="nav-link px-2 link-warning">Features</a></li>
+        <li><a href="#" class="nav-link px-2 link-warning">Pricing</a></li>
+        <li><a href="#" class="nav-link px-2 link-warning">FAQs</a></li>
+        <li><a href="#" class="nav-link px-2 link-warning">About</a></li>
     </ul>
 
     <div class="col-md-3 text-end">
-        <button type="button" class="btn btn-outline-success me-2">Login</button>
-        <button type="button" class="btn btn-success">Sign-up</button>
+        <button type="button" class="btn btn-outline-dark me-2">Login</button>
+        <button type="button" class="btn btn-dark">Sign-up</button>
     </div>
 </header>
-
+<div class="container col-md-10 px-4 py-5">
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+        <div class="col-10 col-sm-8 col-lg-6">
+            <img src="assets/img/NutriTrendz.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700"
+                 height="500"
+                 loading="lazy">
+        </div>
+        <div class="col-lg-6">
+            <h1 class="display-5 fw-bold lh-1 mb-3">NutriTrendz : les dernières actualités en
+                nutrition !</h1>
+            <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                <button type="button" class="btn btn-dark btn-lg px-4 me-md-2">Primary</button>
+            </div>
+        </div>
+    </div>
+</div>
 <script type="text/javascript" src="lib/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
