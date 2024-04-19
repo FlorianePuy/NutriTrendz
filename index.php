fa026a5f6f3e10fbc8dbad9de8955ce74db6922e
FlorianePuy <77892356+FlorianePuy@users.noreply.github.com>
2024-04-19 14:01:19 +0200

    ajustement d'affichage du texte dans les card

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
     
     $articles = [
             ["title"=>"Le Mythe des Régimes à la Mode : Démystification et Réalité",
-                "content"=>"Découvrez la vérité derrière les régimes à la mode dans notre dernier article sur la nutrition. Des régimes cétogène et paléo au jeûne intermittent, nous explorons les promesses et les réalités de ces approches populaires. Nous démêlons les mythes de la perte de poids rapide et vous aidons à adopter une approche plus saine et plus équilibrée de la nutrition. Rejoignez-nous pour découvrir comment cultiver une relation durable avec la nourriture et atteindre vos objectifs de santé de manière réaliste et durable.",
+                "content"=>"Découvrez la vérité sur les régimes populaires dans notre dernier article nutrition. Des régimes cétogène au jeûne intermittent, nous explorons leurs promesses et réalités. Démêlons les mythes de la perte de poids rapide et adoptons une approche saine et équilibrée de la nutrition. Rejoignez-nous pour une relation durable avec la nourriture et des objectifs de santé réalistes.",
                 "image"=>"myths-diet.png"
             ],
             ["title"=>"Les Aliments Superstars : Quels Aliments Ajouter à Votre Assiette pour une Meilleure Santé ?",
-            "content"=>"Découvrez la vérité derrière les régimes à la mode dans notre dernier article sur la nutrition. Des régimes cétogène et paléo au jeûne intermittent, nous explorons les promesses et les réalités de ces approches populaires. Nous démêlons les mythes de la perte de poids rapide et vous aidons à adopter une approche plus saine et plus équilibrée de la nutrition. Rejoignez-nous pour découvrir comment cultiver une relation durable avec la nourriture et atteindre vos objectifs de santé de manière réaliste et durable.",
+            "content"=>"Découvrez la vérité sur les régimes populaires dans notre dernier article nutrition. Des régimes cétogène au jeûne intermittent, nous explorons leurs promesses et réalités. Démêlons les mythes de la perte de poids rapide et adoptons une approche saine et équilibrée de la nutrition. Rejoignez-nous pour une relation durable avec la nourriture et des objectifs de santé réalistes.",
             "image"=>"myths-diet.png"
             ],
             ["title"=>"Le Véritable Impact des Compléments Alimentaires",
-            "content"=>"Découvrez la vérité derrière les régimes à la mode dans notre dernier article sur la nutrition. Des régimes cétogène et paléo au jeûne intermittent, nous explorons les promesses et les réalités de ces approches populaires. Nous démêlons les mythes de la perte de poids rapide et vous aidons à adopter une approche plus saine et plus équilibrée de la nutrition. Rejoignez-nous pour découvrir comment cultiver une relation durable avec la nourriture et atteindre vos objectifs de santé de manière réaliste et durable.",
+            "content"=>"Découvrez la vérité sur les régimes populaires dans notre dernier article nutrition. Des régimes cétogène au jeûne intermittent, nous explorons leurs promesses et réalités. Démêlons les mythes de la perte de poids rapide et adoptons une approche saine et équilibrée de la nutrition. Rejoignez-nous pour une relation durable avec la nourriture et des objectifs de santé réalistes.",
             "image"=>"myths-diet.png"
             ],
     ];
@@ -20,7 +20,7 @@
     <div class="container col-md-10 px-4 py-5">
         <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
             <div class="col-10 col-sm-8 col-lg-6">
-                <img src="assets/img/NutriTrendz.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700"
+                <img src="assets/img/NutriTrendz.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="500"
                      height="500"
                      loading="lazy">
             </div>
@@ -37,12 +37,12 @@
 </main>
 <h2 class="text-center">Les dernières publications :</h2>
 <section class="card-deck d-flex container-fluid justify-content-around my-md-4">
-    <?php for($i=0;$i<3;$i++) {?>
+    <?php foreach ($articles as $article) {?>
     <div class="card col-md-3">
-        <img src="uploads_articles/myths-diet.png" class="card-img-top" alt="illustration d'article de blog">
+        <img src="uploads_articles/<?= $article['image'];?>" class="card-img-top" alt="illustration d'article de blog">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 class="card-title"><?= $article['title']; ?></h5>
+            <p class="card-text text-break"><?= $article['content'];?></p>
             <a href="#" class="btn btn-warning">Go somewhere</a>
         </div>
     </div>
