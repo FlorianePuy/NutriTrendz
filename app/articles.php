2f73b8893e8953d400ad37b11ca6e8e95c73f4fc
FlorianePuy <77892356+FlorianePuy@users.noreply.github.com>
2024-04-19 14:21:10 +0200

    separation des articles dans un fichier du dossier app

---
+++ app/articles.php
@@ -0,0 +1,15 @@
+<?php
+$articles = [
+    ["title"=>"Le Mythe des Régimes à la Mode : Démystification et Réalité",
+        "content"=>"Découvrez la vérité sur les régimes populaires dans notre dernier article nutrition. Des régimes cétogène au jeûne intermittent, nous explorons leurs promesses et réalités. Démêlons les mythes de la perte de poids rapide et adoptons une approche saine et équilibrée de la nutrition. Rejoignez-nous pour une relation durable avec la nourriture et des objectifs de santé réalistes.",
+        "image"=>"myths-diet.png"
+    ],
+    ["title"=>"Les Aliments Superstars : Quels Aliments Ajouter à Votre Assiette pour une Meilleure Santé ?",
+        "content"=>"Découvrez la vérité sur les régimes populaires dans notre dernier article nutrition. Des régimes cétogène au jeûne intermittent, nous explorons leurs promesses et réalités. Démêlons les mythes de la perte de poids rapide et adoptons une approche saine et équilibrée de la nutrition. Rejoignez-nous pour une relation durable avec la nourriture et des objectifs de santé réalistes.",
+        "image"=>"myths-diet.png"
+    ],
+    ["title"=>"Le Véritable Impact des Compléments Alimentaires",
+        "content"=>"Découvrez la vérité sur les régimes populaires dans notre dernier article nutrition. Des régimes cétogène au jeûne intermittent, nous explorons leurs promesses et réalités. Démêlons les mythes de la perte de poids rapide et adoptons une approche saine et équilibrée de la nutrition. Rejoignez-nous pour une relation durable avec la nourriture et des objectifs de santé réalistes.",
+        "image"=>"myths-diet.png"
+    ],
+];
