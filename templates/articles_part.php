f226fc277a5e247425c7f3b6bd09c12956347aac
FlorianePuy <77892356+FlorianePuy@users.noreply.github.com>
2024-04-29 00:32:20 +0200

    (fix):ajout de sécurité sur l'affichage des articles

--- templates/articles_part.php
+++ templates/articles_part.php
@@ -1,9 +1,10 @@
 
 <div class="card col-md-3">
-    <img src="uploads_articles/<?= $article['image'];?>" class="card-img-top" alt="illustration d'article de blog">
+    <img src="uploads_articles/<?= htmlentities($article['image']);?>" class="card-img-top" alt="illustration
+    d'article de blog">
     <div class="card-body">
-        <h5 class="card-title"><?= $article['title']; ?></h5>
-        <p class="card-text text-break"><?= $article['content_short'];?></p>
-        <a href="actualite.php?id=<?=$key ?>" class="btn btn-warning">Lire les actualités</a>
+        <h5 class="card-title"><?= htmlentities($article['title']); ?></h5>
+        <p class="card-text text-break"><?= htmlentities($article['content_short']);?></p>
+        <a href="actualite.php?id=<?=htmlentities($key) ?>" class="btn btn-warning">Lire les actualités</a>
     </div>
 </div>
