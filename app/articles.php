ad74e9b8abac219ed2b55412fe23ad6e9fbd8b53
FlorianePuy <77892356+FlorianePuy@users.noreply.github.com>
2024-05-04 19:29:19 +0200

    (refactor): requete de getArticles

--- app/articles.php
+++ app/articles.php
@@ -2,11 +2,12 @@
 
  function getArticles (PDO $pdo, int $limit=null):array
  {
+     $sql = "SELECT * FROM articles ORDER BY id DESC";
      if ($limit){
-         $query = $pdo->prepare("SELECT * FROM articles ORDER BY id DESC LIMIT :limit;");
+         $query = $pdo->prepare($sql."LIMIT :limit");
          $query->bindValue(":limit",$limit,PDO::PARAM_INT);
      }else{
-         $query = $pdo->prepare("SELECT * FROM articles ORDER BY id DESC;");
+         $query = $pdo->prepare($sql);
      }
      $query->execute();
      return $query->fetchAll(PDO::FETCH_ASSOC);
