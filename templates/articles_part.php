bf627028dbf94452eddc0a17857be22e97ad8dac
FlorianePuy <77892356+FlorianePuy@users.noreply.github.com>
2024-05-04 18:42:47 +0200

    (fix):cas où l'image n'existe pas pour l'article

--- templates/articles_part.php
+++ templates/articles_part.php
@@ -1,6 +1,13 @@
+<?php
+if ($article['image']=== null){
+    $image = "default-image.png";
+}else {
+    $image=htmlentities($article['image']);
+}
 
+?>
 <div class="card col-md-3">
-    <img src="uploads_articles/<?= htmlentities($article['image']);?>" class="card-img-top" alt="illustration
+    <img src="uploads_articles/<?=$image ?>" class="card-img-top" alt="illustration
     d'article de blog">
     <div class="card-body">
         <h6 class="card-title text-center text-decoration-underline"><?= htmlentities($article['title']); ?></h6>
