904abc27b0e845d09e16e2acd592eb841cac06e9
FlorianePuy <77892356+FlorianePuy@users.noreply.github.com>
2024-05-14 18:55:13 +0200

    (feat):Inscription fonctionnelle

--- app/user.php
+++ app/user.php
@@ -1,5 +1,15 @@
 <?php
+function addUser(PDO $pdo, string $first_name, string $last_name, string $email, string $password):bool {
+    $hashedPassword = password_hash($password,PASSWORD_DEFAULT);
+    $query = $pdo->prepare("INSERT INTO users (email, password, first_name, last_name, role) VALUES (:email,:password,:first_name,:last_name,'user')");
 
+    $query->bindValue(":email",$email);
+    $query->bindValue(":password",$hashedPassword);
+    $query->bindValue(":first_name",$first_name);
+    $query->bindValue(":last_name",$last_name);
+
+    return $query->execute();
+}
 function verifyUserLoginPassword(PDO $pdo, string $email, string $password):bool|array {
     $query = $pdo->prepare("SELECT * FROM users WHERE email=:email");
     $query->bindValue(":email",$email);
--- inscription.php
+++ inscription.php
@@ -1,25 +1,33 @@
 <?php
+    require_once __DIR__.'/app/config.php';
+    require_once __DIR__.'/app/pdo.php';
     require_once __DIR__.'/app/mainMenu.php';
     require_once __DIR__.'/templates/header.php';
+    require_once __DIR__.'/app/user.php';
+
+    if (isset($_POST['addUser'])) {
+        $resultat = addUser($pdo, $_POST['first_name'],$_POST['last_name'],$_POST['email'],$_POST['password']);
+        var_dump($resultat);
+    }
 ?>
 
 <div class="container d-flex flex-column mt-3">
     <h1 class="fw-bold">Inscription</h1>
-    <form class="p-4 d-flex flex-column">
+    <form method="post" class="p-4 d-flex flex-column">
             <label class="form-label mb-3">Prénom
-                <input type="text" class="form-control">
+                <input type="text" class="form-control" name="first_name">
             </label>
             <label class="form-label mb-3">Nom
-                <input type="text" class="form-control">
+                <input type="text" class="form-control" name="last_name">
             </label>
             <label class="form-label mb-3">Email
-                <input type="email" class="form-control">
+                <input type="email" class="form-control" name="email">
             </label>
             <label class="form-label mb-3">Mot de passe
-                <input type="password" class="form-control" >
+                <input type="password" class="form-control" name="password">
             </label>
             <div class="mb-3">
-                <button type="submit" class="btn btn-primary text-light">Enregistrer</button>
+                <button type="submit" class="btn btn-primary text-light" name="addUser">Enregistrer</button>
             </div>
     </form>
 </div>
